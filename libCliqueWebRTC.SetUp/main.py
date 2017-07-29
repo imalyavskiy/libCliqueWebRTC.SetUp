9aa498b344717bfa2674662d5210071933835247
Ilya Malyavskiy <ilya.malyavskiy@gmail.com>
2017-07-29 18:41:20 +0700

    Реализовано редактирование CMakeLIsts.txt в socket.io-client-cpp - удаление строки set(Boost_USE_STATIC_RUNTIME OFF) Реализована сборка socket.io-client-cpp через msbuild

--- libCliqueWebRTC.SetUp/main.py
+++ libCliqueWebRTC.SetUp/main.py
@@ -24,21 +24,21 @@ config = {
     , "source_tag"   : "boost-1.64.0"                      # the tag for the branch to create from
     , "build_targets": # a set of build configurations
         [#targets
-            { "command" : "b2.exe"                                                                                                                     # this step is absolutely mandatory for modular boost
+            { "command" : "b2"                                                                                                                     # this step is absolutely mandatory for modular boost
             , "args"    : "headers" }                                                                                                                  # i.e. taken from git directly
-        ,   { "command" : "b2.exe"
+        ,   { "command" : "b2"
             , "args"    : "--with-system --with-date_time --with-random --with-regex link=static runtime-link=static threading=multi address-model=32" # how to build
             , "src"     : "stage/lib"                                                                                                                  # libraries are here
             , "dst"     : "stage/lib_Win32" }                                                                                                          # move them here
-        ,   { "command" : "b2.exe"
+        ,   { "command" : "b2"
             , "args"    : "--with-system --with-date_time --with-random --with-regex link=static runtime-link=shared threading=multi address-model=32" #
             , "src"     : "stage/lib"                                                                                                                  #
             , "dst"     : "stage/lib_Win32" }                                                                                                          #
-        ,   { "command" : "b2.exe"
+        ,   { "command" : "b2"
             , "args"    : "--with-system --with-date_time --with-random --with-regex link=static runtime-link=static threading=multi address-model=64" #
             , "src"     : "stage/lib"                                                                                                                  #
             , "dst"     : "stage/lib_Win64" }                                                                                                          #
-        ,   { "command" : "b2.exe"
+        ,   { "command" : "b2"
             , "args"    : "--with-system --with-date_time --with-random --with-regex link=static runtime-link=shared threading=multi address-model=64" #
             , "src"     : "stage/lib"                                                                                                                  #
             , "dst"     : "stage/lib_Win64" }                                                                                                          #
@@ -61,12 +61,12 @@ config = {
                 , "args"    : "clean -fx -d"}, #clean up local git repo
                 { "command" : "perl" 
                 , "args"    : "Configure VC-WIN32 no-asm --prefix=./../openssl_libs/Win32"}, # configure the feature in order to build the target
-                { "command" : "cmd.exe" 
+                { "command" : "cmd" 
                 , "args"    : "/c"
                 , "rel_args": ["ms/do_ms.bat"]}, # another one configuration step
-                { "command" : "cmd.exe"
+                { "command" : "cmd"
                 , "args"    : "/c nmake -f ms/ntdll.mak"}, # building step
-                { "command" : "cmd.exe"
+                { "command" : "cmd"
                 , "args"    : "/c nmake -f ms/ntdll.mak install"}, # installing step the location of installation has been set previousely see --prefix at few lines above
             ],
             [#steps
@@ -78,12 +78,12 @@ config = {
                 , "args"    : "clean -fx -d"}
             ,   { "command" : "perl"
                 , "args"    : "Configure VC-WIN64A --prefix=./../openssl_libs/Win64"}
-            ,   { "command" : "cmd.exe"
+            ,   { "command" : "cmd"
                 , "args"    : "/c"
                 , "rel_args": ["ms/do_win64a.bat"]} # this step requires ml64.exe
-            ,   { "command" : "cmd.exe"
+            ,   { "command" : "cmd"
                 , "args"    : "/c nmake -f ms/ntdll.mak"}
-            ,   { "command" : "cmd.exe"
+            ,   { "command" : "cmd"
                 , "args"    : "/c nmake -f ms/ntdll.mak install"}
             ]
         ]
@@ -101,30 +101,102 @@ config = {
             [#steps
                 { "command" : "git"
                 , "args"    : ["clean -fx -d"]}, #clean up local git repo
+                { "command" : "edit_file" 
+                , "file"    : "CMakeLists.txt"
+                , "rm_str"  : "set(Boost_USE_STATIC_RUNTIME OFF)"
+                },
                 { "command" : "cmake"
                 , "args"    : 
                     [ "-G \"Visual Studio 15 2017 Win64\""
                     , "./", "-DBOOST_ROOT=\"./../boost/\""
                     , "-DBOOST_LIBRARYDIR=\"./../boost/stage/lib_Win64/\""
                     , "-DOPENSSL_ROOT_DIR=\"./../openssl_libs/Win64/\""
+                    , "-DCMAKE_CXX_FLAGS=/D_WEBSOCKETPP_NOEXCEPT_"
+                    , "-DBoost_USE_STATIC_RUNTIME=ON"
                     ] 
+                },
+                { "upd_env" : "Path"
+                , "append"  : "C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/MSBuild/15.0/Bin/amd64"},
+                { "upd_env" : "PATH"
+                , "append"  : "C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/MSBuild/15.0/Bin/amd64"},
+#                { "command" : "msbuild"
+#                , "args"    : 
+#                    [ "sioclient.vcxproj"
+#                    , "/t:Rebuild"
+#                    , "/p:Configuration=Debug;Platform=x64;OutDir=./../socket.io-client-cpp_libs/Win64/Debug"
+#                    ]
+#                },
+                { "command" : "msbuild"
+                , "args"    : 
+                    [ "sioclient.vcxproj"
+                    , "/t:Rebuild"
+                    , "/p:Configuration=Release;Platform=x64;OutDir=./../socket.io-client-cpp_libs/Win64/Release"
+                    ]
+                },
+#                { "command" : "msbuild"
+#                , "args"    : 
+#                    [ "sioclient_tls.vcxproj"
+#                    , "/t:Rebuild"
+#                    , "/p:Configuration=Debug;Platform=x64;OutDir=./../socket.io-client-cpp_libs/Win64/Debug"
+#                    ]
+#                },
+                { "command" : "msbuild"
+                , "args"    : 
+                    [ "sioclient_tls.vcxproj"
+                    , "/t:Rebuild"
+                    , "/p:Configuration=Release;Platform=x64;OutDir=./../socket.io-client-cpp_libs/Win64/Release"
+                    ]
                 }
-#            ,   { "command" : "msbuild"
-#                , "args"    : [""]}
             ],
             [#steps
                 { "command" : "git"
                 , "args"    : ["clean -fx -d"]}, #clean up local git repo
+                { "command" : "edit_file" 
+                , "file"    : "CMakeLists.txt"
+                , "rm_str"  : "set(Boost_USE_STATIC_RUNTIME OFF)"
+                },
                 { "command" : "cmake"
-                , "args"    :
+                , "args"    : 
                     [ "-G \"Visual Studio 15 2017\""
                     , "./", "-DBOOST_ROOT=\"./../boost/\""
                     , "-DBOOST_LIBRARYDIR=\"./../boost/stage/lib_Win32/\""
                     , "-DOPENSSL_ROOT_DIR=\"./../openssl_libs/Win32/\""
+                    , "-DCMAKE_CXX_FLAGS=/D_WEBSOCKETPP_NOEXCEPT_"
+                    , "-DBoost_USE_STATIC_RUNTIME=ON"
                     ] 
-                }
-#            ,   { "command" : "msbuild"
-#                , "args"    : [""]}
+                },
+                { "upd_env" : "Path"
+                , "append"  : "C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/MSBuild/15.0/Bin/amd64"},
+                { "upd_env" : "PATH"
+                , "append"  : "C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/MSBuild/15.0/Bin/amd64"},
+#                { "command" : "msbuild"
+#                , "args"    : 
+#                    [ "sioclient.vcxproj"
+#                    , "/t:Rebuild"
+#                    , "/p:Configuration=Debug;Platform=x86;OutDir=./../socket.io-client-cpp_libs/Win32/Debug"
+#                    ]
+#                },
+                { "command" : "msbuild"
+                , "args"    : 
+                    [ "sioclient.vcxproj"
+                    , "/t:Rebuild"
+                    , "/p:Configuration=Release;Platform=x86;OutDir=./../socket.io-client-cpp_libs/Win32/Release"
+                    ]
+                },
+#                { "command" : "msbuild"
+#                , "args"    : 
+#                    [ "sioclient_tls.vcxproj"
+#                    , "/t:Rebuild"
+#                    , "/p:Configuration=Debug;Platform=x86;OutDir=./../socket.io-client-cpp_libs/Win32/Debug"
+#                    ]
+#                },
+                { "command" : "msbuild"
+                , "args"    : 
+                    [ "sioclient_tls.vcxproj"
+                    , "/t:Rebuild"
+                    , "/p:Configuration=Release;Platform=x86;OutDir=./../socket.io-client-cpp_libs/Win32/Release"
+                    ]
+                }            
             ],
             [#steps
                 { "command" : "git"
@@ -145,6 +217,79 @@ config = {
 
 log = log_tools.Logger()
 git = git_tools.Client(log)
+
+def is_executable(name, env=None):
+    env_path = os.environ.get("Path")
+    if env is not None and ("Path" in env or "PATH" in env):
+        if "Path" in env:
+            env_path = env["Path"]
+        else:
+            env_path = env["PATH"]
+        
+    if env_path is None:
+        env_path = os.environ.get("PATH")
+    
+    if env_path is None:
+        return False
+
+    entries = env_path.split(";")
+    for entry in entries:
+        if not entry.endswith("\\") and not entry.endswith("/"):
+            entry+="\\"
+        
+        path_to_check = []
+        if name.endswith(".exe") or name.endswith(".bat") or name.endswith(".com"):
+            path_to_check.append(entry+name)
+        else:
+            path_to_check.append(entry+name+".exe")
+            path_to_check.append(entry+name+".com")
+            path_to_check.append(entry+name+".bat")
+        
+        for path in path_to_check:
+            if os.path.isfile(path) and os.access(path, os.X_OK):
+                return True
+    return False
+
+def edit_file(dir, params):
+    if "file" not in params: 
+        return False
+    if "rm_str" not in params:
+        return False
+
+    in_file = open(dir+"/"+params["file"], "r")
+    if in_file is None:
+        return False
+
+    out_file = open(dir+"/"+params["file"]+".tmp", "w")
+    if out_file is None:
+        return False
+
+    while True:
+        line = in_file.readline()
+        if line == "":
+            break
+        elif params["rm_str"] in line:
+            continue
+        else:
+            out_file.write(line)
+
+    in_file.close()
+    out_file.close()
+
+    os.remove(dir+"/"+params["file"])
+    os.rename(dir+"/"+params["file"]+".tmp", dir+"/"+params["file"])
+
+functions = {"edit_file" : edit_file}
+
+def is_function(name):
+    if name in functions:
+        return True
+    return False
+
+def get_function(name):
+    if name in functions:
+        return functions[name]
+    return None
 
 def clone_one(target_dir, resource_name, source_url, target_branch=None, source_tag=None):
     location = target_dir+"/"+resource_name
@@ -254,8 +399,8 @@ def build_openssl(target_dir):
                     if "prepend" in step:
                         _env[step["upd_env"]] = "{0};{1}".format(step["prepend"], _env[step["upd_env"]]) 
                     if "append" in step:
-                        if _env[step["upd_env"]].endswith(";"):
-                            _env[step["upd_env"]] += step["append"]
+                        if not _env[step["upd_env"]].endswith(";"):
+                            _env[step["upd_env"]] += ";"
                         _env[step["upd_env"]] += step["append"] + ";"
             elif "command" in step and "args" in step:
                 args = step["args"]
@@ -270,15 +415,28 @@ def build_openssl(target_dir):
 def build_socketio(target_dir):
     socketio_config = config["socket.io-client-cpp"]
     socketio_root_dir = target_dir + "/socket.io-client-cpp"
-
+    _env = os.environ.copy()
     for build_target in socketio_config["build_targets"]:
         for step in build_target:
-            arguments = ""
-            for arg in step["args"]:
-                if len(arguments) != 0:
-                    arguments+=" "
-                arguments += arg
-            result = git_tools.run(log, step["command"], arguments, cwd=socketio_root_dir)
+            if "command" in step and is_executable(step["command"], _env):
+                arguments = ""
+                for arg in step["args"]:
+                    if len(arguments) != 0:
+                        arguments+=" "
+                    arguments += arg
+                result = git_tools.run(log, step["command"], arguments, cwd=socketio_root_dir, env=_env)
+            elif "command" in step and is_function(step["command"]):
+                fun = get_function(step["command"])
+                fun(socketio_root_dir, step)
+                pass
+            elif "upd_env" in step and step["upd_env"] in _env:
+                if "prepend" in step:
+                    _env[step["upd_env"]] = "{0};{1}".format(step["prepend"], _env[step["upd_env"]]) 
+                if "append" in step:
+                    if not _env[step["upd_env"]].endswith(";"):
+                        _env[step["upd_env"]] += ";"
+                    _env[step["upd_env"]] += step["append"] + ";"
+            pass
         pass
     pass
 
